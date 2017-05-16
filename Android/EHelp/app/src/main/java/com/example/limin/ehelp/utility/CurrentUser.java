44be44db0c59c9ea9a1c6634d41da1d5c8b00ab4
yongningfu <535802703@qq.com>
2017-05-17 02:46:08 +0800

    加入网络类创建

---
+++ Android/EHelp/app/src/main/java/com/example/limin/ehelp/utility/CurrentUser.java
@@ -0,0 +1,10 @@
+package com.example.limin.ehelp.utility;
+
+/**
+ * Created by Administrator on 2017/05/17.
+ */
+
+public class CurrentUser {
+    public static int userId = -1;
+    public static String cookie = "";
+}
