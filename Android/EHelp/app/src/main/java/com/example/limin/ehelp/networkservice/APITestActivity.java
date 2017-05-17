1ea0470eadd79f0abfce84c68cf51a67b3e7936a
yongningfu <535802703@qq.com>
2017-05-17 16:30:14 +0800

    完成点击demo

--- Android/EHelp/app/src/main/java/com/example/limin/ehelp/networkservice/APITestActivity.java
+++ Android/EHelp/app/src/main/java/com/example/limin/ehelp/networkservice/APITestActivity.java
@@ -6,15 +6,25 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.limin.ehelp.R;
 
 public class APITestActivity extends AppCompatActivity {
 
+    Button sendCode;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apitest);
+        sendCode = (Button) findViewById(R.id.sendCode);
+        sendCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(APITestActivity.this, "api", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
-
 }
