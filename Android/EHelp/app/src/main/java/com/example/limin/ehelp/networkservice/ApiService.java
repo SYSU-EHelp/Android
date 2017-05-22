bebef9b8015c8ddcfbef4f6368b6d03c983af2de
yongningfu <535802703@qq.com>
2017-05-22 21:18:36 +0800

    完成回答问题

--- Android/EHelp/app/src/main/java/com/example/limin/ehelp/networkservice/ApiService.java
+++ Android/EHelp/app/src/main/java/com/example/limin/ehelp/networkservice/ApiService.java
@@ -16,8 +16,13 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
 
 /**
  * Created by Administrator on 2017/05/17.
@@ -66,6 +71,10 @@ public interface ApiService {
     Call<EmptyResult> requestAddQuestion(@Field("title") String  title,
                                          @Field("description") String description);
 
+
+    @PATCH("/api/questions/{question_id}")
+    Call<EmptyResult> requestAnswerQuestion(@Path("question_id") int question_id,
+                                            @Query("answer") String answer);
 
 
     static final Interceptor interceptor = new Interceptor() {
