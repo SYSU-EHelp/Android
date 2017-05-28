4043fb0c5ad3a0bff6518ed73f2209067a40593c
胡南 <1067861587@qq.com>
2017-05-28 12:46:28 +0800

    完成头像选取功能，紧急联系人添加功能

--- Android/EHelp/app/src/main/java/com/example/limin/ehelp/EmergencyContact.java
+++ Android/EHelp/app/src/main/java/com/example/limin/ehelp/EmergencyContact.java
@@ -1,8 +1,12 @@
 package com.example.limin.ehelp;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -19,6 +23,7 @@ public class EmergencyContact extends AppCompatActivity {
     private ListView lv;
     private Button btn_back;
     private TextView title;
+    private Button add;
     private TextView next;
     private TextView message;
     private String[] username = new String[] {"张三","李四"};
@@ -31,17 +36,46 @@ public class EmergencyContact extends AppCompatActivity {
         setTitle();
 
         lv = (ListView)findViewById(R.id.emergency);
+        add = (Button)findViewById(R.id.addContact);
 
-        List<Map<String, Object>> contact = new ArrayList<>();
+        final List<Map<String, Object>> contact = new ArrayList<>();
         for (int i = 0; i < username.length; i++) {
             Map<String, Object> temp = new LinkedHashMap<>();
             temp.put("username",username[i]);
             temp.put("phone", phone[i]);
             contact.add(temp);
         }
-        SimpleAdapter simpleAdapter = new SimpleAdapter(this, contact, R.layout.contact_item,
+        final SimpleAdapter simpleAdapter = new SimpleAdapter(this, contact, R.layout.contact_item,
                 new String[] {"username", "phone"}, new int[] {R.id.username, R.id.phone});
         lv.setAdapter(simpleAdapter);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater inflater = getLayoutInflater();
+                View layout = inflater.inflate(R.layout.add_dialog, (ViewGroup)findViewById(R.id.add_dialog));
+                final EditText addname = (EditText)layout.findViewById(R.id.add_name);
+                final EditText addphone = (EditText)layout.findViewById(R.id.add_phone);
+                new AlertDialog.Builder(EmergencyContact.this).setTitle("添加紧急联系人")
+                        .setView(layout)
+                        .setPositiveButton("确认", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String add_name = addname.getText().toString();
+                                String add_phone = addphone.getText().toString();
+                                Map<String, Object> temp2 = new LinkedHashMap<>();
+                                temp2.put("username", add_name);
+                                temp2.put("phone", add_phone);
+                                contact.add(temp2);
+                                simpleAdapter.notifyDataSetChanged();
+                                Toast.makeText(getApplicationContext(),"添加联系人成功",Toast.LENGTH_SHORT).show();
+
+                            }
+                        })
+                        .setNegativeButton("取消",null).show();
+            }
+        });
+
     }
 
     private void setTitle() {
