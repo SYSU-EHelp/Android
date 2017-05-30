f4bc06e6fb7b69caa92cc0fc23058af4f00aa710
胡南 <1067861587@qq.com>
2017-05-30 14:36:06 +0800

    完成紧急联系人添加按钮，以及个人信息头像选取

--- Android/EHelp/app/src/main/java/com/example/limin/ehelp/Information.java
+++ Android/EHelp/app/src/main/java/com/example/limin/ehelp/Information.java
@@ -1,12 +1,22 @@
 package com.example.limin.ehelp;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.FileNotFoundException;
 
 /**
  * Created by Tompkins on 2017/5/9.
@@ -18,18 +28,46 @@ public class Information extends AppCompatActivity {
     private TextView next;
     private EditText username;
     private EditText phone;
+    private ImageView icon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
         setTitle();
+
+        icon = (ImageView)findViewById(R.id.icon);
+        icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                //设置格式为图片可以打开所有图片
+                intent.setType("image:/*");
+                startActivityForResult(intent,1);
+            }
+        });
         //二次载入需要加载个人信息
         /*
         username.setText();
         phone.setText();
         */
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            Uri uri = data.getData();
+            Log.e("uri", uri.toString());
+            ContentResolver cr = this.getContentResolver();
+            try {
+                Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));
+                icon.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     private void setTitle() {
