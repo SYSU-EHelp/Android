09b649ff8965b5065d3fed9e8e9051d6bd642c56
Tongyunzhao <314342613@qq.com>
2017-06-27 19:16:24 +0800

    关闭求救定位功能

--- Android/EHelp/app/src/main/java/com/example/limin/ehelp/EmergencyHelp.java
+++ Android/EHelp/app/src/main/java/com/example/limin/ehelp/EmergencyHelp.java
@@ -46,7 +46,7 @@ public class EmergencyHelp extends AppCompatActivity{
         setContentView(R.layout.activity_emergency_help);
 
         //初始化定位
-        initLocation();
+        //initLocation();
 
         tv_helpernum = (TextView)findViewById(R.id.tv_helpernum);
         exit = (Button)findViewById(R.id.exit_help);
@@ -90,7 +90,7 @@ public class EmergencyHelp extends AppCompatActivity{
                         return;
                     }
                     //ToastUtils.show(EmergencyHelp.this, new Gson().toJson(response.body()));
-                    Toast.makeText(EmergencyHelp.this, curLocStr, Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(EmergencyHelp.this, curLocStr, Toast.LENGTH_SHORT).show();
                 }
                 @Override
                 public void onFailure(Call<EmptyResult> call, Throwable t) {
@@ -105,88 +105,88 @@ public class EmergencyHelp extends AppCompatActivity{
         }
     };
 
-    private void startLocation(){
-        // 设置定位参数
-        locationClient.setLocationOption(locationOption);
-        // 启动定位
-        locationClient.startLocation();
-    }
-
-    private void stopLocation(){
-        // 停止定位
-        locationClient.stopLocation();
-    }
-
-    private void initLocation(){
-        //初始化client
-        locationClient = new AMapLocationClient(this.getApplicationContext());
-        locationOption = getDefaultOption();
-        //设置定位参数
-        locationClient.setLocationOption(locationOption);
-        // 设置定位监听
-        locationClient.setLocationListener(locationListener);
-    }
-
-    /**
-     * 定位监听
-     */
-    AMapLocationListener locationListener = new AMapLocationListener() {
-        @Override
-        public void onLocationChanged(AMapLocation location) {
-            if (null != location) {
-                // 更新当前位置
-                //curLoc = location;
-
-                StringBuffer sb = new StringBuffer();
-                //errCode等于0代表定位成功，其他的为定位失败，具体的可以参照官网定位错误码说明
-                if(location.getErrorCode() == 0){
-                    sb.append(location.getAddress());
-                    sb.append(location.getDistrict());
-                    sb.append(location.getStreet());
-                    sb.append(location.getDescription());
-                } else {
-                    //定位失败
-//                    sb.append("错误码:" + location.getErrorCode() + "\n");
-//                    sb.append("错误信息:" + location.getErrorInfo() + "\n");
-//                    sb.append("错误描述:" + location.getLocationDetail() + "\n");
-//                    Toast.makeText(EditHelpActivity.this, "定位失败", Toast.LENGTH_SHORT).show();
-                }
-
-                curLocStr = sb.toString();
-
-            } else {
-                Toast.makeText(EmergencyHelp.this, "定位失败", Toast.LENGTH_SHORT).show();
-            }
-        }
-    };
-
-    private AMapLocationClientOption getDefaultOption(){
-        AMapLocationClientOption mOption = new AMapLocationClientOption();
-        mOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);//可选，设置定位模式，可选的模式有高精度、仅设备、仅网络。默认为高精度模式
-        mOption.setGpsFirst(false);//可选，设置是否gps优先，只在高精度模式下有效。默认关闭
-        mOption.setHttpTimeOut(30000);//可选，设置网络请求超时时间。默认为30秒。在仅设备模式下无效
-        mOption.setInterval(10000);//可选，设置定位间隔。默认为2秒
-        mOption.setNeedAddress(true);//可选，设置是否返回逆地理地址信息。默认是true
-        mOption.setOnceLocation(false);//可选，设置是否单次定位。默认是false
-        mOption.setOnceLocationLatest(false);//可选，设置是否等待wifi刷新，默认为false.如果设置为true,会自动变为单次定位，持续定位时不要使用
-        AMapLocationClientOption.setLocationProtocol(AMapLocationClientOption.AMapLocationProtocol.HTTP);//可选， 设置网络请求的协议。可选HTTP或者HTTPS。默认为HTTP
-        mOption.setSensorEnable(false);//可选，设置是否使用传感器。默认是false
-        mOption.setWifiScan(true); //可选，设置是否开启wifi扫描。默认为true，如果设置为false会同时停止主动刷新，停止以后完全依赖于系统刷新，定位位置可能存在误差
-        mOption.setLocationCacheEnable(true); //可选，设置是否使用缓存定位，默认为true
-        return mOption;
-    }
-
-    @Override
-    protected void onPause() {
-        stopLocation();
-        super.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-        startLocation();
-        super.onResume();
-    }
+//    private void startLocation(){
+//        // 设置定位参数
+//        locationClient.setLocationOption(locationOption);
+//        // 启动定位
+//        locationClient.startLocation();
+//    }
+//
+//    private void stopLocation(){
+//        // 停止定位
+//        locationClient.stopLocation();
+//    }
+//
+//    private void initLocation(){
+//        //初始化client
+//        locationClient = new AMapLocationClient(this.getApplicationContext());
+//        locationOption = getDefaultOption();
+//        //设置定位参数
+//        locationClient.setLocationOption(locationOption);
+//        // 设置定位监听
+//        locationClient.setLocationListener(locationListener);
+//    }
+//
+//    /**
+//     * 定位监听
+//     */
+//    AMapLocationListener locationListener = new AMapLocationListener() {
+//        @Override
+//        public void onLocationChanged(AMapLocation location) {
+//            if (null != location) {
+//                // 更新当前位置
+//                //curLoc = location;
+//
+//                StringBuffer sb = new StringBuffer();
+//                //errCode等于0代表定位成功，其他的为定位失败，具体的可以参照官网定位错误码说明
+//                if(location.getErrorCode() == 0){
+//                    sb.append(location.getAddress());
+//                    sb.append(location.getDistrict());
+//                    sb.append(location.getStreet());
+//                    sb.append(location.getDescription());
+//                } else {
+//                    //定位失败
+////                    sb.append("错误码:" + location.getErrorCode() + "\n");
+////                    sb.append("错误信息:" + location.getErrorInfo() + "\n");
+////                    sb.append("错误描述:" + location.getLocationDetail() + "\n");
+////                    Toast.makeText(EditHelpActivity.this, "定位失败", Toast.LENGTH_SHORT).show();
+//                }
+//
+//                curLocStr = sb.toString();
+//
+//            } else {
+//                Toast.makeText(EmergencyHelp.this, "定位失败", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//    };
+//
+//    private AMapLocationClientOption getDefaultOption(){
+//        AMapLocationClientOption mOption = new AMapLocationClientOption();
+//        mOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);//可选，设置定位模式，可选的模式有高精度、仅设备、仅网络。默认为高精度模式
+//        mOption.setGpsFirst(false);//可选，设置是否gps优先，只在高精度模式下有效。默认关闭
+//        mOption.setHttpTimeOut(30000);//可选，设置网络请求超时时间。默认为30秒。在仅设备模式下无效
+//        mOption.setInterval(10000);//可选，设置定位间隔。默认为2秒
+//        mOption.setNeedAddress(true);//可选，设置是否返回逆地理地址信息。默认是true
+//        mOption.setOnceLocation(false);//可选，设置是否单次定位。默认是false
+//        mOption.setOnceLocationLatest(false);//可选，设置是否等待wifi刷新，默认为false.如果设置为true,会自动变为单次定位，持续定位时不要使用
+//        AMapLocationClientOption.setLocationProtocol(AMapLocationClientOption.AMapLocationProtocol.HTTP);//可选， 设置网络请求的协议。可选HTTP或者HTTPS。默认为HTTP
+//        mOption.setSensorEnable(false);//可选，设置是否使用传感器。默认是false
+//        mOption.setWifiScan(true); //可选，设置是否开启wifi扫描。默认为true，如果设置为false会同时停止主动刷新，停止以后完全依赖于系统刷新，定位位置可能存在误差
+//        mOption.setLocationCacheEnable(true); //可选，设置是否使用缓存定位，默认为true
+//        return mOption;
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        stopLocation();
+//        super.onPause();
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        startLocation();
+//        super.onResume();
+//    }
 
 
 }
